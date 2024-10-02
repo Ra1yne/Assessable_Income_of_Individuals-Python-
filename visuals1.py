c0da64c40850785aa9c159ba1abee581a91f9898
Ra1yne <1hchuazhijie@gmail.com>
2024-10-02 23:48:21 +0800

    rename

--- visuals1.py
+++ visuals1.py
@@ -16,16 +16,16 @@ year = pivot_tax_group_data.index
 width = 0.35
 x = np.arange(len(year))
 
+plt.figure(figsize=(10, 6))
 
 plt.bar(x-width/2, pivot_tax_group_data['Non-Taxable Group'], width, label='Non-Taxable Group', color='skyblue')
 plt.bar(x+width/2, pivot_tax_group_data['Taxable Group'], width, label='Taxable Group', color='orange')
 
-plt.figure(figsize=(10, 6))
 
 plt.xlabel('Year')
 plt.ylabel('Assessable Income')
 plt.title('Assessable Income For Different Tax Groups')
 
-plt.xticks(x, year)
 plt.legend()
+plt.xticks(x, year)
 plt.show()
